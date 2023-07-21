8d271f1a99ba5f9a82039adc9b59b06411389693
milind patil <milindp.tagline@gmail.com>
2023-07-21 16:38:51 +0530

    the project commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root';
+import { rootloader, rootAction } from './routes/root';
+
+import ErrorPage from './routes/Error-page';
+import Contact, { contactLoader, deleteAction, starAction } from './routes/Contact';
+import EditContact, { editAction } from './routes/edit';
+import Index from './routes';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootloader,
+    action: rootAction,
+
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+
+          { index: true, element: <Index /> },
+          {
+            path: "/contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: starAction
+            // errorElement: <ErrorPage />,
+          },
+          {
+            path: "/contacts/:contactId?/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+
+            // errorElement: <ErrorPage />,
+          },
+          {
+            path: "/contacts/:contactId/destroy",
+            action: deleteAction,
+            // errorElement: <ErrorPage />,
+          },
+        ]
+      }
+    ]
+  },
+
+])
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
